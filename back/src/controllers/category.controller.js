e67f08b82b63ace50170f6c5f67a2137f62f1d98
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2023-02-21 20:47:30 -0300

    feat: category controller

--- back/src/controllers/category.controller.js
+++ back/src/controllers/category.controller.js
@@ -1,4 +1,41 @@
+const { serverError, badRequest, ok } = require('../helpers/http.helper.js');
 const { CategoryService } = require('../services/category.service.js');
-const categoryService = new CategoryService();
-class CategoryController {}
+const categoryService = new CategoryService({ category: prisma.todo });
+class CategoryController {
+  async create(request, response) {
+    try {
+      const error = Validator.checkRequiredFields({
+        requiredFields: ['name'],
+        body: request.body,
+      });
+      if (error) return badRequest({ error, response });
+      const { name } = request.body;
+      const category = await categoryService.create({ name });
+      return ok({ category, response });
+    } catch (error) {
+      return serverError({ error, response });
+    }
+  }
+
+  async findAll(request, response) {
+    try {
+    } catch (error) {
+      return serverError({ error, response });
+    }
+  }
+
+  async delete(request, response) {
+    try {
+    } catch (error) {
+      return serverError({ error, response });
+    }
+  }
+
+  async findTodosById(request, response) {
+    try {
+    } catch (error) {
+      return serverError({ error, response });
+    }
+  }
+}
 module.exports = new CategoryController();
